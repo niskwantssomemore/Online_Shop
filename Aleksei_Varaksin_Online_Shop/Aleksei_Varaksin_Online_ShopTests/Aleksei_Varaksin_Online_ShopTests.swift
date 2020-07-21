a80dc02b88ff0140587dbfcf7bb2c17f193034c5
niskwantssomemore <52360473+niskwantssomemore@users.noreply.github.com>
2020-07-22 02:59:40 +0300

    Version-1-0

---
+++ Aleksei_Varaksin_Online_Shop/Aleksei_Varaksin_Online_ShopTests/Aleksei_Varaksin_Online_ShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  Aleksei_Varaksin_Online_ShopTests.swift
+//  Aleksei_Varaksin_Online_ShopTests
+//
+//  Created by Aleksei Niskarav on 22/07/2020.
+//  Copyright © 2020 Aleksei Niskarav. All rights reserved.
+//
+
+import XCTest
+@testable import Aleksei_Varaksin_Online_Shop
+
+class Aleksei_Varaksin_Online_ShopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
